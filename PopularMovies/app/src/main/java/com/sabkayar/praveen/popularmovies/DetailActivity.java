6d5d44d64cba2b687f0e7b81b33ad1dee7882adb
Praveen <pkchmits@gmail.com>
2019-07-10 06:06:32 +0530

    Data binding implemented without using butterknife library

--- PopularMovies/app/src/main/java/com/sabkayar/praveen/popularmovies/DetailActivity.java
+++ PopularMovies/app/src/main/java/com/sabkayar/praveen/popularmovies/DetailActivity.java
@@ -1,56 +1,45 @@
 package com.sabkayar.praveen.popularmovies;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import com.sabkayar.praveen.popularmovies.databinding.ActivityDetailBinding;
 import com.squareup.picasso.Picasso;
 
 import java.text.DecimalFormat;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String MOVIE_EXTRA = "MOVIE_EXTRA";
     private static final String LOG_TAG = DetailActivity.class.getSimpleName();
 
-    @BindView(R.id.tv_title)
-    TextView mTitleTextView;
-    @BindView(R.id.imv_poster)
-    ImageView mPosterImageView;
-    @BindView(R.id.tv_year)
-    TextView mYearTextView;
-    @BindView(R.id.tv_date_month)
-    TextView mDateMonthTextView;
-    @BindView(R.id.tv_rating)
-    TextView mRatingTextView;
-    @BindView(R.id.tv_overview)
-    TextView mOverViewTextView;
-
+    ActivityDetailBinding mBinding;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        ButterKnife.bind(this);
+        //Data binding implementation
+        mBinding= DataBindingUtil.setContentView(this,R.layout.activity_detail);
 
         Movie movie = getIntent().getParcelableExtra(MOVIE_EXTRA);
 
 
-        mTitleTextView.setText(movie.getTitle());
+        mBinding.tvTitle.setText(movie.getTitle());
         Picasso.get().load(NetworkUtils.getImageAbsolutePath(movie.getMoviePosterRelativePath()))
-                .placeholder(R.drawable.progress_animation).error(R.drawable.error_placeholder).into(mPosterImageView);
-        mYearTextView.setText(Utils.getFormattedDate(movie.getReleaseDate(), "yyyy"));
-        mDateMonthTextView.setText(Utils.getFormattedDate(movie.getReleaseDate(), "MMM, dd"));
+                .placeholder(R.drawable.progress_animation).error(R.drawable.error_placeholder).into(mBinding.imvPoster);
+        mBinding.tvYear.setText(Utils.getFormattedDate(movie.getReleaseDate(), "yyyy"));
+        mBinding.tvDateMonth.setText(Utils.getFormattedDate(movie.getReleaseDate(), "MMM, dd"));
 
         DecimalFormat decimalFormat = new DecimalFormat("0.0");
-        mRatingTextView.append(decimalFormat.format(Double.valueOf(movie.getAverageVoting())));
-        mRatingTextView.append("/10.0");
-        mOverViewTextView.setText(movie.getMovieOverView());
+        mBinding.tvRating.append(decimalFormat.format(Double.valueOf(movie.getAverageVoting())));
+        mBinding.tvRating.append("/10.0");
+        mBinding.tvOverview.setText(movie.getMovieOverView());
 
         Log.d(LOG_TAG, movie.getTitle() +
                 "\n" + movie.getAverageVoting() +
--- PopularMovies/app/src/main/java/com/sabkayar/praveen/popularmovies/MainActivity.java
+++ PopularMovies/app/src/main/java/com/sabkayar/praveen/popularmovies/MainActivity.java
@@ -3,6 +3,7 @@ package com.sabkayar.praveen.popularmovies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,42 +12,38 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.sabkayar.praveen.popularmovies.databinding.ActivityMainBinding;
 
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements MovieAdapter.OnListItemClickListener {
     private static final String SORT_BY_POPULARITY = "popular";
     private static final String SORT_BY_RATING = "top_rated";
-    private RecyclerView mRecyclerView;
     private MovieAdapter mAdapter;
-    private ConstraintLayout mConstraintLayout;
-    private ConstraintLayout mNoInternetConstraintLayout;
-    private ProgressBar mProgressBar;
     private FetchMovieDataTask mFetchMovieDataTask;
     private URL mURL;
 
+    ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mRecyclerView = findViewById(R.id.recycler_view);
-        mConstraintLayout = findViewById(R.id.cl_progress_layout);
-        mNoInternetConstraintLayout = findViewById(R.id.cl_no_internet);
-        mProgressBar = findViewById(R.id.determinateBar);
+        mBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
 
         int numOfColumns = Utils.calculateNoOfColumns(this, 100);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, numOfColumns);
         ItemOffsetDecoration itemDecoration = new ItemOffsetDecoration(this, R.dimen.item_offset);
-        mRecyclerView.addItemDecoration(itemDecoration);
+        mBinding.recyclerView.addItemDecoration(itemDecoration);
 
-        mRecyclerView.setLayoutManager(gridLayoutManager);
-        mRecyclerView.setHasFixedSize(true);
+        mBinding.recyclerView.setLayoutManager(gridLayoutManager);
+        mBinding.recyclerView.setHasFixedSize(true);
 
         mAdapter = new MovieAdapter(this);
-        mRecyclerView.setAdapter(mAdapter);
+        mBinding.recyclerView.setAdapter(mAdapter);
 
         makeServerCall(SORT_BY_POPULARITY);
 
@@ -62,9 +59,9 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnLi
     @Override
     protected void onRestart() {
         super.onRestart();
-        if (mNoInternetConstraintLayout.getVisibility() == View.VISIBLE) {
+        if (mBinding.clNoInternet.getVisibility() == View.VISIBLE) {
             if (NetworkUtils.isConnected(this)) {
-                mNoInternetConstraintLayout.setVisibility(View.GONE);
+                mBinding.clNoInternet.setVisibility(View.GONE);
                 startCountDownForCheckingInternet();
                 mFetchMovieDataTask.execute(mURL);
             }
@@ -77,10 +74,10 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnLi
             }
 
             public void onFinish() {
-                if (mProgressBar.getProgress() == 10) {
+                if (mBinding.determinateBar.getProgress() == 10) {
                     Toast.makeText(MainActivity.this, "Please check your internet connectivity", Toast.LENGTH_LONG).show();
-                    mConstraintLayout.setVisibility(View.GONE);
-                    mNoInternetConstraintLayout.setVisibility(View.VISIBLE);
+                    mBinding.clProgressLayout.setVisibility(View.GONE);
+                    mBinding.clNoInternet.setVisibility(View.VISIBLE);
                     mFetchMovieDataTask.cancel(true);
                 }
             }
@@ -113,13 +110,13 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnLi
 
     private void makeServerCall(String sortBy) {
         mURL = NetworkUtils.createUrl(sortBy);
-        mFetchMovieDataTask = new FetchMovieDataTask(mAdapter, mConstraintLayout, mProgressBar);
+        mFetchMovieDataTask = new FetchMovieDataTask(mAdapter, mBinding.clProgressLayout, mBinding.determinateBar);
         if (NetworkUtils.isConnected(this)) {
-            mNoInternetConstraintLayout.setVisibility(View.GONE);
+            mBinding.clNoInternet.setVisibility(View.GONE);
             startCountDownForCheckingInternet();
             mFetchMovieDataTask.execute(mURL);
         } else {
-            mNoInternetConstraintLayout.setVisibility(View.VISIBLE);
+            mBinding.clNoInternet.setVisibility(View.VISIBLE);
         }
     }
 }
